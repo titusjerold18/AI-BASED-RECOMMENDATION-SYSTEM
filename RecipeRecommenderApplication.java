f55c43db55fde0788cebb2fb6054306c4361b71f
titusjerold18 <jos.tjerold18@gmail.com>
2025-07-14 22:48:18 +0530

    Create RecipeRecommenderApplication.java

---
+++ RecipeRecommenderApplication.java
@@ -0,0 +1,14 @@
+package com.asvikaa.recommender.recipeRecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeRecommenderApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RecipeRecommenderApplication.class, args);
+	}
+
+}
